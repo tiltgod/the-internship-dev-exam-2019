72ca7e4018b6cf850af7d8e8a5e1f2e7576f9c0b
tiltgod <59070084@it.kmitl.ac.th>
2019-01-18 00:10:09 +0700

    select interpreter and write some docstring

---
+++ weather/weather.py
@@ -0,0 +1,5 @@
+""" -*- coding: python3 -*-
+    author: Napasin Hongnger"""
+""" weather
+    โปรแกรมแปลงข้อมูลสภาพอากาศ จาก Format XML เป็น json ใน Terminal/Console (Command Line Interface)
+    โปรแกรมจะรับ Input เป็นไฟล์สภาพอากาศสกุล xml และรีเทิร์น Output เป็น ไฟล์สภาพอากาศสกุล json"""

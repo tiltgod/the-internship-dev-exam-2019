aab50d5444210774cb7ec56ec1196bb17d5be762
tiltgod <59070084@it.kmitl.ac.th>
2019-01-23 11:54:44 +0700

    making progress
    thing to do:
    -fix bug
        - no input but get score
        - about duplicates
    -refactor
    -edit word list
        - must has only 2 duplicates

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -19,20 +19,18 @@ file_path_1 = absolute_path + "/animal_wordlist.txt"
 file_path_2 = absolute_path + "/pcgames_wordlist.txt"
 file_path_3 = absolute_path + "/netflix_original_wordlist.txt"
 
-life = 10
+life = 5
 word_order = 0
 point_max = 0
 guesses = ""
 word = ''
-while life == 10:
-    try: 
-        category = int(input())
-        break
-        sys.exit(0)
-    except:
-        category = 0
-        print("That's not valid option!! Exit the game.")
-        sys.exit(0)
+
+try: 
+    category = int(input())
+except:
+    category = 0
+    print("That's not valid option!! Exit the game.")
+    sys.exit(0)
 
 if category == 1:
     wordlist_1 = open(file_path_1, 'r')
@@ -42,7 +40,7 @@ if category == 1:
     hint = secondline_of_category_1.split('/')
     wordlist_1.close()
     word_order += randint(0,4)
-    word += answer[word_order]
+    word += "rabbit"
     print("Hint: "+ hint[word_order])
     #print(answer[word_order])
 elif category == 2:
@@ -72,19 +70,59 @@ else:
     sys.exit(0)
     
 point = 0
-point_max += len(word) * 10
+point_max += len(word) 
+print(point_max)
 print(word)
 
+def areCharactersUnique(s): 
+      
+    # An integer to store presence/absence  
+    # of 26 characters using its 32 bits 
+    checker = 0
+      
+    for i in range(len(s)): 
+          
+        val = ord(s[i]) - ord('a') 
+          
+        # If bit corresponding to current  
+        # character is already set 
+        if (checker & (1 << val)) > 0: 
+            return False
+          
+        # set bit in checker  
+        checker |= (1 << val) 
+          
+    return True
+      
+# Driver code 
+s = word
+if areCharactersUnique(s): 
+    print("Yes") 
+else: 
+    print("No") 
+
 while life > 0:
+    failed = 0
+    turn = 0
+    for char in word:
+        if char in guesses:
+            print(char, end = " ")
+        else:
+            print("_", end = " ")
+        
+    print(str(point) + "\n", end = " ")
+    
     guess = input()
     guesses += guess
-    for char in word:
-        if char in guess:
-            print(char)
-        else:
-            print(point)
 
-
-
-    
-
+    if guess in word:
+        point += 1
+    else:
+        life = life - 1
+    if life == 0:
+        print("wp")
+        break
+    if point == point_max:
+        print("well played")
+        print(point)
+        break

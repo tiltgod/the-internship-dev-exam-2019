8693309d58f18f6a2f1b8a98498f043168f4553e
tiltgod <59070084@it.kmitl.ac.th>
2019-01-23 22:10:30 +0700

    bugs are fixed

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -21,9 +21,11 @@ file_path_3 = absolute_path + "/netflix_original_wordlist.txt"
 
 life = 5
 word_order = 0
+duplicate_check = 0
+point = 0
 point_max = 0
 guesses = ""
-word = ''
+word = ""
 
 try: 
     category = int(input())
@@ -40,39 +42,37 @@ if category == 1:
     hint = secondline_of_category_1.split('/')
     wordlist_1.close()
     word_order += randint(0,4)
-    word += "rabbit"
-    print("Hint: "+ hint[word_order])
-    #print(answer[word_order])
+    word += answer[word_order].strip()
+    print("\n"+"Hint: "+ hint[word_order]+"\n")
 elif category == 2:
-    wordlist_2 = open(file_path_2, 'r')
-    firstline_of_category_2 = wordlist_2.readline()
-    secondline_of_category_2 = wordlist_2.readline()
-    answer = firstline_of_category_2.split('/')
-    hint = secondline_of_category_2.split('/')
-    wordlist_2.close()
-    word_order += randint(0,4)
-    word += answer[word_order]
-    print("Hint: "+ hint[word_order])
-    #print(answer[word_order])
+    # wordlist_2 = open(file_path_2, 'r')
+    # firstline_of_category_2 = wordlist_2.readline()
+    # secondline_of_category_2 = wordlist_2.readline()
+    # answer = firstline_of_category_2.split('/')
+    # hint = secondline_of_category_2.split('/')
+    # wordlist_2.close()
+    # word_order += randint(0,4)
+    # word += answer[word_order]
+    # print("\n"+"Hint: "+ hint[word_order]+"\n")
+    print("sorry, this part not available yet.")
 elif category == 3:
-    wordlist_3 = open(file_path_3, 'r')
-    firstline_of_category_3 = wordlist_3.readline()
-    secondline_of_category_3 = wordlist_3.readline()
-    answer = firstline_of_category_3.split('/')
-    hint = secondline_of_category_3.split('/')
-    wordlist_3.close()
-    word_order += randint(0,4)
-    word += answer[word_order]
-    print("Hint: "+ hint[word_order])
-    #print(answer[word_order])
+    # wordlist_3 = open(file_path_3, 'r')
+    # firstline_of_category_3 = wordlist_3.readline()
+    # secondline_of_category_3 = wordlist_3.readline()
+    # answer = firstline_of_category_3.split('/')
+    # hint = secondline_of_category_3.split('/')
+    # wordlist_3.close()
+    # word_order += randint(0,4)
+    # word += answer[word_order]
+    # print("\n"+"Hint: "+ hint[word_order]+"\n")
+    print("sorry, this part not available yet.")
 else:
     print("That is not an option!! exit the game.")
     sys.exit(0)
     
-point = 0
+
 point_max += len(word) 
-print(point_max)
-print(word)
+
 
 def areCharactersUnique(s): 
       
@@ -93,38 +93,48 @@ def areCharactersUnique(s):
         checker |= (1 << val) 
           
     return True
-      
 # Driver code 
 s = word
-if areCharactersUnique(s): 
-    print("Yes") 
+if areCharactersUnique(s):
+    pass
 else: 
-    print("No") 
+    duplicate_check += 1
+if duplicate_check == 1:
+    point_max = point_max - 1
 
 while life > 0:
     failed = 0
-    turn = 0
-    for char in word:
-        if char in guesses:
-            print(char, end = " ")
-        else:
-            print("_", end = " ")
-        
-    print(str(point) + "\n", end = " ")
-
-    guess = input()
-    if guess.isalpha():
-        guesses += guess
-        if guess in word:
-            point += 1
-        else:
-            life = life - 1
-    else:
-        print("pls insert a alphabet")
     if life == 0:
         print("wp")
         break
     if point == point_max:
         print("well played")
         print(point)
+        break
+    for char in word:
+        if char in guesses:
+            print(char, end = " ")
+        else:
+            print("_", end = " ")
+        
+    print("Score: " +str(point) + " " + "Life: " + str(life) + " " + "guessed: " +(guesses) +"\n", end = " ")
+    print("\n")
+
+    guess = input()
+    if guess.isalpha() and len(guess) == 1:
+        guesses += guess
+        if guess in word:
+            point += 1
+        else:
+            life = life - 1
+    else:
+        print("\n"+"pls insert a alphabet."+"\n")
+    if life == 0:
+        print("Sorry not today."+"\n")
+        break
+    if point == point_max:
+        for j in word:
+            print(j, end = " ")
+        print("Score: " +str(point) + " " + "Life: " + str(life) + " " + "guessed: " +(guesses) +"\n")
+        print("You won, Good game well played."+"\n")
         break

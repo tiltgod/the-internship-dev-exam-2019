578696e3ef169d56e0f498d801ffb956944a76bd
tiltgod <59070084@it.kmitl.ac.th>
2019-01-24 11:36:23 +0700

    write comment

--- weather/weather.py
+++ weather/weather.py
@@ -6,19 +6,21 @@ import json
 import xmltodict
 import os
 
-xml_input = input()
-with open(xml_input, 'r') as raw_xml:
+xml_input = input() # Get a file as input
+filename_w_ext = os.path.basename(xml_input) # Get basename from file
+filename = os.path.splitext(filename_w_ext)[0] # Get only filename from basename
+
+with open(xml_input, 'r') as raw_xml: # Read a file and get all of the file as string
     xml_str = raw_xml.read()
 
-converted_json = json.dumps(xmltodict.parse(xml_str, attr_prefix='', cdata_key=''), indent=4)
+converted_json = json.dumps(xmltodict.parse(xml_str, attr_prefix='', cdata_key=''), indent=4) # Converting file by using xmltodict library and get rid of prefixes
 
-filename_w_ext = os.path.basename(xml_input)
-filename = os.path.splitext(filename_w_ext)[0]
-json_output = filename + ".json"
+json_output = filename + ".json" # create output 's filename
 print(json_output)
 
-with open(json_output, 'w') as json_writer:
+with open(json_output, 'w') as json_writer: # write a json file
     json_writer.write(converted_json)
+
 
 
  
